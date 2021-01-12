0d48b8a2ed2da51f740c2afb5668fe14bd569b99
palak555 <75022281+palak555@users.noreply.github.com>
2021-01-12 14:45:11 +0530

    Add files via upload

---
+++ pinkBox.js
@@ -0,0 +1,26 @@
+class PinkBox{
+    constructor(x, y) {
+        var options = {
+             isStatic:false,
+            'restitution':0.5,
+            'friction':0.1,
+            'density':1.0
+        }
+        this.box = Bodies.rectangle(x, y, 40, 50, options);
+        this.width = 40;
+        this.height = 50;
+        
+        World.add(world, this.box);
+      }
+      display(){
+        var pos= this.box.position;
+        rectMode(CENTER)
+        stroke("black")
+        strokeWeight(2.3);
+        fill("pink")
+        rect(pos.x,pos.y,40,50)
+
+        
+      }    
+        
+}
